9c712bd0ff5d316d45244962076569b23b5ae3c9
David <dk.shin1992@gmail.com>
2017-02-06 14:49:44 +1100

    create config

--- config_060217_test.m
+++ config_060217_test.m
@@ -7,7 +7,7 @@ verbose=1;
 %%% Raw data handling
 % files -  data file
 configs.files.path='C:\Users\HE BEC\Documents\lab\quantum-depletion\exp1\d';    % path to unindexed data file (e.g. 'a\b\datadir\$DATA_FNAME_TOKEN$')
-configs.files.id=1:50;          % file id numbers to use for analysis
+configs.files.id=1:1500;          % file id numbers to use for analysis
 configs.files.minCount=100;     % min counts to use for analysis
 
 
@@ -18,7 +18,12 @@ configs.rot_angle=0.61;     % angle in rad (exp default should be 0.61)
 %   XY window is applied after rotation
 configs.window{1}=[0.56 0.575];      % T [s]
 configs.window{2}=[-35e-3,30e-3];    % X [m]
-configs.window{3}=[-40e-3,25e-3];    % Y [m]
+configs.window{3}=[-40e-3,28e-3];    % Y [m]
+
+
+%%% HISTOGRAMMING
+configs.nbin=1000;
+
 
 %%% ALGORITHM CONFIGS
 % DO NOT ADJUST
