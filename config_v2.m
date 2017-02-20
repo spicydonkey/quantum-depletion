3d6ef86ec5f9b9661f4fc3931db8c72f4247398a
David <dk.shin1992@gmail.com>
2017-02-20 13:40:38 +1100

    create version2 - switch params, new fit

---
+++ config_v2.m
@@ -0,0 +1,186 @@
+% Config file for quantum depletion in far-field momentum distribution
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% data files iterate through predefined experimental param set
+% allow for different analysis depending on param set used
+%
+% log-log transform of data and a linear fit to obtain power law
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%% GENERAL
+verbose=2;
+
+%%% Raw data handling
+% files -  data file
+configs.files.path='C:\Users\HE BEC\Documents\lab\quantum-depletion\exp5\d';    % path to unindexed data file (e.g. 'a\b\datadir\$DATA_FNAME_TOKEN$')
+configs.files.id=1:500;          % file id numbers to use for analysis
+configs.files.minCount=100;     % min counts to use for analysis
+
+
+% XY plane rotation to align to trap geometry
+configs.rot_angle=0.61;     % angle in rad (exp default should be 0.61)
+
+% TXY window - region of interest ( [] --> no crop )
+%   XY window is applied after rotation
+%   window liberally for very long k-tail
+configs.window{1}=[0.50 0.64];      % T [s]
+configs.window{2}=[-45e-3,40e-3];    % X [m]
+configs.window{3}=[-50e-3,38e-3];    % Y [m]
+
+
+% Param set
+configs.paramset=2;     % TODO: number of params iterated
+
+
+% BEC locator - used for accurate location of condensate
+configs.bec.txy_pos=[0.5715,-3.45e-3,-10e-3];       % approx bec location (get from DLD front panel)
+configs.bec.box_fwidth=[15e-3,20e-3,40e-3];     % txy full-width for box capt (be liberal-takes mean)
+
+
+%%% Experimental consts 
+configs.const.detect_qe=0.1;      % detector quantum efficiency
+configs.const.hbar=1.05457e-34;     % hbar [m^2kg/s]
+configs.const.m_He=6.646476e-27;    % mass of helium (kg)
+configs.const.tof=0.416;    % TOF for free-fall from trap to DLD
+
+
+%% Quantum depletion specific
+%%% Angular integration over radial profile - Cylindrical sector
+configs.cylsect_theta_lims=pi+[-pi/8,pi/8];    % in-radial plane angle lims for angular averaging
+configs.cylsect_trans_hwidth=4e-3;          % transverse averaging half width [m]
+
+%% 1D slice
+% % TODO - document usage properly
+% % counts captured along a 1D line-slice from well "below" condensate to centre
+% configs.slice.mincount=500;     % minimum count in 1D slice to pass
+% configs.slice.cyl_orient=1;     % slice through Z-axis (1:Z,2:X,3:Y)
+% configs.slice.cyl_rad=1e-3;     % cyl radius [m]
+% configs.slice.cyl_hgt=280e-3;    % cyl height [m]
+% 
+% % build cylinder dim
+% configs.slice.cyl_dim=[configs.slice.cyl_rad,configs.slice.cyl_hgt];
+% 
+% % build cylinder centre such that slice ends at condensate centre
+% configs.slice.cyl_cent=zeros(1,3);
+% configs.slice.cyl_cent(configs.slice.cyl_orient)=-0.5*configs.slice.cyl_hgt;
+
+%%% Angular averaging
+% Rotate around X-direction (radial plane is YZ)
+% TODO - feature isn't implemented - just uses {1}
+configs.axial_rot_angle{1}=linspace(-pi/8,pi/8,20);     % point -Z
+configs.axial_rot_angle{2}=pi+linspace(-pi/8,pi/8,20);  % point +Z
+
+
+% %% Background density
+% % configs.do_bgd_calc=1;
+% % +Z direction
+% configs.bgd_cyl_dir{1}=1;
+% configs.bgd_cyl_orient{1}=1;       
+% configs.bgd_disp{1}=[0,0.027,0.027];    % T,X,Y disp to do background analysis
+% configs.bgd_cyl_rad{1}=5e-3;
+% configs.bgd_cyl_hgt{1}=k2r(20*1e6);     % cylinder oriented in +Z direction
+% 
+% % -Z direction
+% configs.bgd_cyl_dir{2}=-1;     % cylinder oriented in -Z direction
+% configs.bgd_cyl_orient{2}=1;       % cylinder oriented in -Z direction
+% configs.bgd_disp{2}=[0,0.027,0.027];    % T,X,Y disp to do background analysis
+% configs.bgd_cyl_rad{2}=5e-3;
+% configs.bgd_cyl_hgt{2}=k2r(20*1e6);    
+% 
+% % build cylinder params
+% for i=1:length(configs.bgd_cyl_orient)
+%     configs.bgd_cyl_dim{i}=[configs.bgd_cyl_rad{i},configs.bgd_cyl_hgt{i}];
+%     configs.bgd_cyl_cent{i}=configs.bgd_disp{i};
+%     configs.bgd_cyl_cent{i}(configs.bgd_cyl_orient{i})=configs.bgd_cyl_cent{i}(configs.bgd_cyl_orient{i})+0.5*configs.bgd_cyl_dir{i}*configs.bgd_cyl_hgt{i};
+% end
+
+
+%% Histogramming
+configs.hist.nbin=100;   % used for real space and linear-k distribution
+% TODO - (log-spaced) bin edges to use in k-space --> simplifies anisotropic
+% analysis
+configs.hist.ed_lgk=logspace(5,7.3,1000);   % 10^X [m^-1 == 1e-6 um^-1]
+
+
+%% Fit to large-k tail
+%% TODO
+% %%% Thermal depletion
+% % fitting region
+% configs.fit.k_lim_thermal=[2.5e6 6e6];      % bound k to use for fit [m^-1]
+% % fitting function
+% configs.fit.fun_negpowk_thermal='y~(x1^())';    % negative-power function
+% configs.fit.fun_coefname_thermal={'A','alpha'};         % function coefficient names
+% % initial conditions
+% configs.fit.param0_thermal=[1e10,4];   % (A, alpha)
+% % fit options
+% configs.fit.opt_thermal=statset('TolFun',1e-15,...
+%     'TolX',1e-15,...
+%     'MaxIter',1e6,...
+%     'UseParallel',1,...
+%     'Display','off');
+
+% %%% Quantum depletion - power law
+% % fitting region
+% configs.fit.k_min=8e6;      % lower bound k to use for fit [m^-1]
+% % fitting function
+% configs.fit.fun_negpowk='y~A*(x1^(-alpha))';    % negative-power function
+% configs.fit.fun_coefname={'A','alpha'};         % function coefficient names
+% % initial conditions
+% configs.fit.param0=[1e10,4.0];   % (A, alpha)
+% % fit options
+% configs.fit.opt=statset('TolFun',1e-30,...
+%     'TolX',1e-30,...
+%     'MaxIter',1e8,...
+%     'UseParallel',1,...
+%     'Display','off');
+
+%%% Quantum depletion - loglog transformed linear
+% fitting region
+configs.fit.k_min=5e6;      % lower bound k to use for fit [m^-1]
+% fitting function
+configs.fit.fun_negpowk='y~A-alpha*x1';     % linearised fitting function
+configs.fit.fun_coefname={'A','alpha'};    	% function coefficient names
+% initial conditions
+configs.fit.param0=[10,4.0];   % (A, alpha)
+% fit options
+configs.fit.opt=statset('TolFun',1e-30,...
+    'TolX',1e-30,...
+    'MaxIter',1e8,...
+    'UseParallel',1,...
+    'Display','off');
+
+
+%% Limits
+%%% Scale
+% size of cloud limited by detector resolution (0.1mm) and BEC (68mm=10um-1)
+configs.limit.r_com=1e-3*[0.1,68];      % [m]
+configs.limit.k_com=1e-6*[0.1,10];      % [m^-1]
+
+%%% Density 
+% TODO set more accurately - currently rounded to power of 10
+% evaluated from dark counts (~6.5e-3 um3) and saturation (~100 mm-^3)
+configs.limit.det_dark_nr=1e9*1e-5;  	% dark ct density in r [m^-3]
+configs.limit.det_sat_nr=1e9*1e3;       % sat ct density in r
+
+configs.limit.det_dark_nk=1e-18*1e-3;   % dark ct density in k [m^3]
+configs.limit.det_sat_nk=1e-18*1e5;     % sat ct density in k
+
+% create limits for plotting
+configs.limit.rdensity=[configs.limit.det_dark_nr,configs.limit.det_sat_nk];
+configs.limit.kdensity=[configs.limit.det_dark_nk,configs.limit.det_sat_nk];
+
+
+%% ALGORITHM CONFIGS
+% DO NOT ADJUST
+configs.files.saveddata=[configs.files.path,'_data.mat'];     % path to store saved data
+configs.files.archive=[configs.files.path,'_archive'];   % dir to archive folder
+configs.files.dirout=[configs.files.path,'_output\'];      % output directory
+
+% create dir if necessary
+if ~exist(configs.files.archive,'dir')
+    warning('Archive directory %s does not exist. Creating directory...',configs.files.archive);
+    mkdir(configs.files.archive);
+end
+if ~exist(configs.files.dirout,'dir')
+    warning('Output directory %s does not exist. Creating directory...',configs.files.dirout);
+    mkdir(configs.files.dirout);
+end
---
+++ main_quantum_depletion_v2.m
@@ -0,0 +1,392 @@
+% Quantum depletion
+% DKS 06/02/2017
+
+clear all; close all; clc;
+
+%%% USER INPUTS
+path_config='C:\Users\HE BEC\Documents\MATLAB\quantum-depletion\config_v2.m';
+path_param_log='C:\Users\HE BEC\Documents\lab\quantum-depletion\exp4\log_test.txt';
+
+% vars to save to output
+vars_save={'path_config',...
+    'zxy_0','files_out',...
+    'r_1D','k_1D',...
+    'r_perp_area','k_perp_area'...
+    'hist_r1D','nden_r1D','nden_k1D',...
+    'hist_lgk1D','nden_lgk1D',...
+    'nk4',...
+    'nden_lgk_avg','nden_lgk_std','nden_lgk_se',...
+    'k4_fit'...
+    'hist_k_cyl_1D','nden_k_cyl_1D',...
+    'nden_k_cyl_avg','nden_k_cyl_std','nden_k_cyl_se',...
+    'k4cyl_fit'...
+    };
+
+
+%% Main
+t_main_start=tic;
+
+% load config
+run(path_config);
+
+% load misc params
+hbar=configs.const.hbar;
+m_He=configs.const.m_He;
+tof=configs.const.tof;
+vz=9.8*tof;     % atom free-fall vert v at detector hit for T-to-Z conversion;
+detQE=configs.const.detect_qe;
+
+hist_r1D.binN=configs.hist.nbin;    % get number of bins to set up auto
+
+% Load TXY data and crop to region of interest
+[txy_raw,files_out]=loadExpData(configs,verbose);
+nShot_all=size(txy_raw,1);
+
+% Load log file with param settings ID
+% table is row vectors of [file id, param set id]
+% TODO: make modular to accept datetime format, comment, etc
+% table_param_id=csvread(path_param_log);
+% TODO: fix this HACK - assumes 1,2 repeat
+table_param_id=zeros(nShot_all,2);
+table_param_id(:,1)=1:nShot_all;
+table_param_id(:,2)=mod(table_param_id(:,1)-1,2)+1;
+
+
+%% Pre-processing
+txy_cat=cell(configs.paramset,1);     % TXY data categorised
+settings_idx=cell(configs.paramset,1);
+
+% categorise TXY files
+for idx=1:configs.paramset
+    settings_idx{idx}=find(table_param_id(:,2)==idx);
+%     txy_0{idx}=cell(sum(table_param_id(:,2)==idx),1);
+%     txy_0{idx}=cell(numel(settings_idx{idx}),1);
+    txy_cat{idx}=txy_raw(settings_idx{idx});  % populate this param set
+end
+
+% %% Categorize Raw TXY data
+% txy_raw_cat=cell(configs.paramset,1);
+% % TODO: make general
+% counter1=1;
+% counter2=1;
+% for i=1:size(txy_raw,1)
+%     if mod(files_out.id_ok(i),2)==1
+%         % Cat 1
+%         txy_raw_cat{1}{counter1,1}=txy_raw{i};
+%         counter1=counter1+1;
+%     else
+%         % Cat 2
+%         txy_raw_cat{2}{counter2,1}=txy_raw{i};
+%         counter2=counter2+1;
+%     end
+% end
+
+% %% TODO: QUICK HACK FOR NOW
+% % update all new data into old var settings
+% txy_raw=txy_raw_cat{1};     % do same analysis but use only the param set #1
+% 
+% % re-evaluate
+% nShot_all=size(txy_raw,1);
+
+
+%%% Prepare complete set of oscillation cancelled ZXY data
+% convert TXY to ZXY centred around condensate (oscillation compensation)
+txy_0=cell(configs.paramset,1);        % oscillation compensated txy counts
+zxy_0=cell(configs.paramset,1);        % T-Z conversion
+
+for paridx=1:configs.paramset
+    txy_0{paridx}=cell(size(txy_cat{paridx}));    % initialise cell array size
+    zxy_0{paridx}=txy_0{paridx};
+end
+
+%% Param set #1
+txy_temp=txy_cat{1};      % the signal BEC set
+nShot_temp=numel(txy_temp);     % number of shots in set
+
+% Build BEC locator box
+bec_boxlim=cell(1,3);
+for i=1:3
+    bec_boxlim{i}=configs.bec.txy_pos(i)+configs.bec.box_fwidth(i)*[-0.5,0.5];
+end
+
+% txy_cent=zeros(nShot_raw,3);    % centre of all captured TXY
+bec_cent=zeros(nShot_temp,3);    % approx condensate centre from average of captured
+num_txy_raw=zeros(1,nShot_temp); % number of counts in loaded shot
+
+% build complete oscil cancelled data in the region of interest
+for i=1:nShot_temp
+%     txy_cent(i,:)=mean(txy_raw{i},1);       % approx condensate centre from average of captured
+    % get mean position of counts captured in user specified box for
+    % locating BEC
+    [~,~,~,bec_cent(i,:)]=boxcull(txy_temp{i},bec_boxlim);
+    
+    num_txy_raw(i)=size(txy_temp{i},1);      % number of counts in this shot
+    txy_0{1}{i}=txy_temp{i}-repmat(bec_cent(i,:),[num_txy_raw(i),1]);   % centre around self-average
+    
+    % T-Z conversion
+    zxy_0{1}{i}=txy_0{1}{i};
+    zxy_0{1}{i}(:,1)=zxy_0{1}{i}(:,1)*vz;     % TOF - dz at time of detection
+end
+
+% get mean absolute position of BEC
+% TODO - maybe cull bad shots
+avg_bec_cent=nanmean(bec_cent,1);
+std_bec_cent=nanstd(bec_cent,1);
+
+
+%% Param set #2
+txy_temp=txy_cat{2};    % the background counts set (RF sweep off)
+nShot_temp=numel(txy_temp);     % number of shots in set
+num_txy_raw=zeros(1,nShot_temp); % number of counts in loaded shot
+
+% Centre to mean BEC position
+for i=1:nShot_temp
+    num_txy_raw(i)=size(txy_temp{i},1);      % number of counts in this shot
+    txy_0{2}{i}=txy_temp{i}-repmat(avg_bec_cent,[num_txy_raw(i),1]);   % centre around BEC avg
+    
+    % T-Z conversion
+    zxy_0{2}{i}=txy_0{2}{i};
+    zxy_0{2}{i}(:,1)=zxy_0{2}{i}(:,1)*vz;     % TOF - dz at time of detection
+end
+
+
+%% Plot far-field ZXY (summary)
+nShotSumm=50;   % number of shots to plot as summary
+if min(size(zxy_0{1},1),size(zxy_0{2},1))<nShotSumm
+    nShotSumm=min(size(zxy_0{1},1),size(zxy_0{2},1));
+end
+if verbose>1
+    h_zxy_ff=figure();
+    
+    subplot(1,2,1);
+    plot_zxy(zxy_0{1}(1:nShotSumm),20,'k');    
+    title('Condensate point cloud (Summary)');
+    xlabel('X [m]'); ylabel('Y [m]'); zlabel('Z [m]');
+    view(3);
+    axis equal;
+    
+    subplot(1,2,2);
+    plot_zxy(zxy_0{2}(1:nShotSumm),20,'k');    
+    title('Condensate point cloud (Summary)');
+    xlabel('X [m]'); ylabel('Y [m]'); zlabel('Z [m]');
+    view(3);
+    axis equal;
+    
+    % save plot
+    fname_str='zxy_ff';
+    saveas(h_zxy_ff,[configs.files.dirout,fname_str,'.png']);
+    saveas(h_zxy_ff,[configs.files.dirout,fname_str,'.fig']);
+end
+
+
+%% Cylindrical sector for angular averaging
+% get cylindrical sector params
+cyl_dtheta=diff(configs.cylsect_theta_lims);
+cyl_dktrans=2*r2k(configs.cylsect_trans_hwidth);
+
+for idxparam=1:configs.paramset
+    
+    % convert BEC centered real-space counts to cylindrically symmetric coord system
+    % FORMAT: R_CYL = (rad_plane,theta,dist_transverse)
+    R0_cyl{idxparam}=cell(size(zxy_0{idxparam}));
+    nShot_this=size(zxy_0{idxparam},1);
+    for i=1:nShot_this
+        R0_cyl{idxparam}{i}=zeros(size(zxy_0{idxparam}{i}));
+        R0_cyl{idxparam}{i}(:,1)=sqrt(sum(zxy_0{idxparam}{i}(:,[1,3]).^2,2));   % get in-plane radius [m]
+        R0_cyl{idxparam}{i}(:,2)=atan2(zxy_0{idxparam}{i}(:,1),zxy_0{idxparam}{i}(:,3))+pi/2;     % theta origin is pointing "down" in Z [-pi,pi]
+        R0_cyl{idxparam}{i}(:,3)=zxy_0{idxparam}{i}(:,2);       % transverse direction is in X [m]
+    end
+    
+    % convert cylindrical R to k-space
+    k_cyl{idxparam}=R0_cyl{idxparam};
+    for i=1:nShot_this
+        k_cyl{idxparam}{i}(:,1)=r2k(k_cyl{idxparam}{i}(:,1));
+        k_cyl{idxparam}{i}(:,3)=r2k(k_cyl{idxparam}{i}(:,3));
+    end
+    
+    % crop to region of interest for binning k
+    k_cyl_sect{idxparam}=cell(size(k_cyl{idxparam}));
+    for i=1:nShot_this
+        k_cyl_sect{idxparam}{i}=k_cyl{idxparam}{i};  % get everything
+        
+        % cull to angular lims
+        theta_tmp=k_cyl_sect{idxparam}{i}(:,2);	% [-pi,pi]
+        theta_tmp=wrapTo2Pi(theta_tmp-configs.cylsect_theta_lims(1));   % zero angle to start of lim
+        ind_tmp=(theta_tmp<cyl_dtheta);   % indices lying in defined angular sector
+        
+        k_cyl_sect{idxparam}{i}=k_cyl_sect{idxparam}{i}(ind_tmp,:);   % cull
+        
+        % cull to transverse width
+        k_trans_tmp=k_cyl_sect{idxparam}{i}(:,3);  % transverse k
+        ind_tmp=(abs(k_trans_tmp)<r2k(configs.cylsect_trans_hwidth));   % indices lying in defined angular section
+        
+        k_cyl_sect{idxparam}{i}=k_cyl_sect{idxparam}{i}(ind_tmp,:);   % cull
+    end
+    
+    % Get 1D-k
+    k_1D_cyl{idxparam}=cell(nShot_this,1);    % get shot-wise cell array of k
+    for i=1:nShot_this
+        k_1D_cyl{idxparam}{i}=k_cyl_sect{idxparam}{i}(:,1);
+    end
+    % k_1D_cyl=vertcat(k_cyl_sect{:});
+    % k_1D_cyl=k_1D_cyl(:,1);
+    
+    %COLLATE!
+    k_1D_cyl{idxparam}=vertcat(k_1D_cyl{idxparam}{:});
+    
+    %%% Histogram
+    % get hist params
+    hist_k_cyl_1D.binEdge{idxparam}=configs.hist.ed_lgk;     % get log-spaced edges
+    hist_k_cyl_1D.binCent{idxparam}=sqrt(hist_k_cyl_1D.binEdge{idxparam}(1:end-1).*hist_k_cyl_1D.binEdge{idxparam}(2:end));   % GEOM avg bin centres
+    
+    dk_cyl_volume{idxparam}=cyl_dktrans*cyl_dtheta*(hist_k_cyl_1D.binCent{idxparam}).*diff(hist_k_cyl_1D.binEdge{idxparam});     % phase space volume in k
+    
+    % hist_k_cyl_1D.N=cell(nShot_raw,1);
+    % nden_k_cyl_1D=cell(nShot_raw,1);
+    % for i=1:nShot_raw
+    %     hist_k_cyl_1D.N{i}=histcounts(k_1D_cyl{i},hist_k_cyl_1D.binEdge);   % single-shot
+    %     nden_k_cyl_1D{i}=(hist_k_cyl_1D.N{i})./(detQE*dk_cyl_volume);  % normalised for: QE, phase space volume
+    % end
+    hist_k_cyl_1D.N{idxparam}=histcounts(k_1D_cyl{idxparam},hist_k_cyl_1D.binEdge{idxparam});     % collated
+    
+    % evaluate number density
+    nden_k_cyl_1D{idxparam}=(hist_k_cyl_1D.N{idxparam})./(nShot_this*detQE*dk_cyl_volume{idxparam});  % normalised for: shot, QE, phase space volume
+end
+
+% subtract background
+nk_bgd_free=nden_k_cyl_1D{1}-nden_k_cyl_1D{2};
+
+if verbose>0    % plot
+    % far-field momentum space (log)
+    h_nk_cyl_1D_log=figure();
+    figure(h_nk_cyl_1D_log);
+    hold on; box on;
+    
+    for idxparam=1:configs.paramset
+        plot(1e-6*hist_k_cyl_1D.binCent{idxparam},...
+            1e18*nden_k_cyl_1D{idxparam},'.-');     % scale units appropriately
+        
+        xlim(1e6*configs.limit.k_com);
+        ylim(1e18*configs.limit.kdensity);
+        set(gca,'xScale','log');
+        set(gca,'yScale','log');
+        
+        grid on;
+        title('1D condensate momentum profile - cylindrical capture');
+        xlabel('$k$ [$\mu$m$^{-1}$]'); ylabel('$n_{\infty}(k)$ [$\mu$m$^3$]');
+    end
+    
+    % plot background subtracted density profile
+    plot(1e-6*hist_k_cyl_1D.binCent{idxparam},...
+            1e18*nk_bgd_free,'-');     % scale units appropriately
+    
+    fname_str='nk_cyl_1D_loglog';
+    saveas(h_nk_cyl_1D_log,[configs.files.dirout,fname_str,'.png']);
+    saveas(h_nk_cyl_1D_log,[configs.files.dirout,fname_str,'.fig']);
+end
+
+% %%% Evaluate nk4
+% nk4_cyl=nden_k_cyl_1D.*((hist_k_cyl_1D.binCent).^4);
+% 
+% if verbose>0    % plot
+%     h_nk4_cyl=figure();
+%     figure(h_nk4_cyl);
+%     
+%     semilogy(1e-6*hist_k_cyl_1D.binCent,nk4_cyl,'*-');
+%     hold on;
+%     
+%     grid on;
+%     ylim([1e8,1e11]);       % y limits to like Clement PRL
+%     xlabel('$k$ [$\mu$m$^{-1}$]');
+%     ylabel('$k^{4}n_{\infty}(k)$ [m$^{-1}$]');
+%     
+%     fname_str='nk4_cyl';
+%     saveas(h_nk4_cyl,[configs.files.dirout,fname_str,'.png']);
+%     saveas(h_nk4_cyl,[configs.files.dirout,fname_str,'.fig']);
+% end
+
+
+%% Statistical summary
+% TODO - do for all other data
+% nden_k_cyl_collated=vertcat(nden_k_cyl_1D{:});   % collated k-density profile for all angles
+% nden_k_cyl_avg=mean(nden_k_cyl_collated,1);     % angular averaged farfield k profile
+% nden_k_cyl_std=std(nden_k_cyl_collated,1);      % standard deviation
+% nden_k_cyl_se=nden_k_cyl_std/sqrt(size(nden_k_cyl_collated,1));	% standard error
+% 
+% % Plot
+% h_nk_cyl_avg=figure();
+% hold on; box on;
+% mseb(1e-6*hist_k_cyl_1D.binCent,1e18*nden_k_cyl_avg,...
+%     1e18*nden_k_cyl_std);  % NOTE: error in shaded error bar when error is larger than mean
+% 
+% % % plot smoothed background count distribution
+% % for i=1:n_iter_bgd
+% %     loglog(1e-6*hist_lgk1D_bgd{i}.binCent,...
+% %         1e18*n_bgd_smooth{i},'.-');     % scale units appropriately
+% % end
+%     
+% xlim(1e6*configs.limit.k_com);
+% ylim(1e18*configs.limit.kdensity);
+% set(gca,'xScale','log');    % loglog scale
+% set(gca,'yScale','log');
+% 
+% grid on;
+% title('Angular averaged - 1D k profile');
+% xlabel('$k$ [$\mu$m$^{-1}$]'); ylabel('$n_{\infty}(k)$ [$\mu$m$^3$]');
+% 
+% fname_str='nk1D_cyl_avg';
+% saveas(h_nk_cyl_avg,[configs.files.dirout,fname_str,'.png']);
+% saveas(h_nk_cyl_avg,[configs.files.dirout,fname_str,'.fig']);
+
+
+%% Fit density profile
+% common
+ratio_extrap=1;
+
+[~,I_qd]=min(abs(hist_k_cyl_1D.binCent{idxparam}-configs.fit.k_min));  % get index from which to fit QD neg-power law
+
+% get fitting data
+k4cyl_fit.QD.k_log=log(hist_k_cyl_1D.binCent{idxparam}(I_qd:end));   
+k4cyl_fit.QD.nk_log=real(log(nk_bgd_free(I_qd:end)));
+k4cyl_fit.QD.nk_log(isinf(k4cyl_fit.QD.nk_log))=NaN;    % infinity to NaN
+
+k4cyl_fit.QD.fit=fitnlm(k4cyl_fit.QD.k_log,k4cyl_fit.QD.nk_log,...
+    configs.fit.fun_negpowk,configs.fit.param0,...
+    'CoefficientNames',configs.fit.fun_coefname,...
+    'Options',configs.fit.opt);
+
+% Summarise fit
+disp(k4cyl_fit.QD.fit);
+
+% build a sample of the fitted model's profile
+% ratio_extrap=1.5;
+k4cyl_fit.QD.k_log_fit=linspace((min(k4cyl_fit.QD.k_log)),max(k4cyl_fit.QD.k_log),1000);   % indep var to evaluate fitted function
+k4cyl_fit.QD.nk_log_fit=feval(k4cyl_fit.QD.fit,k4cyl_fit.QD.k_log_fit);  % evaluate fitted model
+
+% Plot
+figure(h_nk_cyl_1D_log); hold on;
+plot(1e-6*exp(k4cyl_fit.QD.k_log_fit),1e18*exp(k4cyl_fit.QD.nk_log_fit),'k--');
+
+% Save plot
+fname_str='nk_cyl_fit';
+% saveas(h_nk_cyl_avg,[configs.files.dirout,fname_str,'.png']);
+% saveas(h_nk_cyl_avg,[configs.files.dirout,fname_str,'.fig']);
+
+saveas(h_nk_cyl_1D_log,[configs.files.dirout,fname_str,'.png']);
+saveas(h_nk_cyl_1D_log,[configs.files.dirout,fname_str,'.fig']);
+
+
+%% Save data
+for i = 1:length(vars_save)
+    if ~exist(vars_save{i},'var')
+        warning(['Variable "',vars_save{i},'" does not exist.']);
+        continue;
+    end
+    save(configs.files.saveddata,vars_save{i},'-append');     % to overcome version conflict
+end
+
+%% END
+t_main_end=toc(t_main_start);
+disp('-----------------------------------------------');
+fprintf('Total elapsed time (s): %7.1f\n',t_main_end);
+disp('===================ALL TASKS COMPLETED===================');
