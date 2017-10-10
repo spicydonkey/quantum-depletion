cb586a3a2988dcf91ddfa74dc45e3d6c75a31bc8
David <dk.shin1992@gmail.com>
2017-10-10 17:24:15 +1100

    update rf frac characterisation script

--- run_rffrac.m
+++ run_rffrac.m
@@ -1,19 +1,8 @@
-%% Configs
-configs.files.path='\\AMPLPC29\He BEC Archive\EXPERIMENT-DATA\QuantumDepletion\rf_oc_frac\vpp_1300\d';
+% Characterise RF sweep outcoupling fraction
 
-configs.load.id=1:7;
-configs.load.mincount=0;            % min counts in window - 0 for no min
-configs.load.maxcount=Inf;          % max counts in window - Inf for no max
-configs.load.rot_angle=0.61;
-
-configs.load.window{1}=[0.56,0.58];      % T [s]
-configs.load.window{2}=[-35e-3,35e-3];    % X [m]
-configs.load.window{3}=[-35e-3,35e-3];    % Y [m]
-
-% define region for outcoupled atom (mf=0)
-vz=9.81*0.416;
-boxLim={[0.56,0.58]*vz,[-0.01,0.005],[-35e-3,35e-3]};
-
+%% Load config
+path_config='C:\Users\HE BEC\Documents\MATLAB\quantum-depletion\configs\config_rf_0v9';
+run(path_config);
 
 %% Load data
 [txy,fout]=load_txy(configs.files.path,configs.load.id,...
